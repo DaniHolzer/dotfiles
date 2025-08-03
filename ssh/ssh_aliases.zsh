df46b84bfe8a9cdc97d96845bc55487621c061a4
Dani Holzer <holzerdaniel01@gmail.com>
2025-08-03 16:49:23 +0200

    Add alias for fitcrew GitHub SSH key

--- ssh/ssh_aliases.zsh
+++ ssh/ssh_aliases.zsh
@@ -1,8 +1,8 @@
 #------------------------------------------------------------------------------
 #----- SSH Aliases
 
-alias sshe='eval "$(ssh-agent -s)"'                                     # Start SSH-Agent and set its environment variables.
-alias sshpgh='eval "$(ssh-agent -s)" && ssh-add ~/.ssh/personal-github' # Activate personal GitHub SSH.
-alias sshpgl='eval "$(ssh-agent -s)" && ssh-add ~/.ssh/personal-gitlab' # Activate personal GitLab SSH.
-alias sshwgh='eval "$(ssh-agent -s)" && ssh-add ~/.ssh/vds_ubuntu'      # Activate work GitHub SSH.
-alias sshfck='eval "$(ssh-agent -s)" && ssh-add ~/.ssh/fitcrew-ssh-key' # Activate fitcrew GitLab SSH.
+alias sshe='eval "$(ssh-agent -s)"'                                            # Start SSH-Agent and set its environment variables.
+alias sshpgh='eval "$(ssh-agent -s)" && ssh-add ~/.ssh/personal-github'        # Activate personal GitHub SSH.
+alias sshpgl='eval "$(ssh-agent -s)" && ssh-add ~/.ssh/personal-gitlab'        # Activate personal GitLab SSH.
+alias sshfgl='eval "$(ssh-agent -s)" && ssh-add ~/.ssh/fitcrew-ssh-key-gitlab' # Activate fitcrew GitLab SSH.
+alias sshfgh='eval "$(ssh-agent -s)" && ssh-add ~/.ssh/fitcrew-ssh-key-github' # Activate fitcrew GitHub SSH.
